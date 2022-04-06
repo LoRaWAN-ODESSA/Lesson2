a806130006ffca909f6786678f945623f76df094
Admin <sergey_kovalenko2107@ukr.net>
2022-04-06 13:58:52 +0300

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -60,5 +60,26 @@ public class Main {
         client3.printClientInfo();
         country3.printCountryInfo();
         car3.printCarInfo();
+
+        MyObject myFirstObject = new MyObject();
+        myFirstObject.number = 10;
+        myFirstObject.text = "text1";
+
+        MyObject mySecondObject = new MyObject();
+        mySecondObject.number = 20;
+        mySecondObject.text = "text2";
+
+        System.out.println("myFirstObject: number=" + myFirstObject.number + ", text=" + myFirstObject.text);
+        System.out.println("mySecondObject: number=" + mySecondObject.number + ", text=" + mySecondObject.text);
+
+        System.out.println("Static: " + myFirstObject.STATIC_NUMBER + ","
+        + mySecondObject.STATIC_NUMBER + ","
+        + MyObject.STATIC_NUMBER);
+
+        MyObject.STATIC_NUMBER = MyObject.STATIC_NUMBER + 1;
+
+        System.out.println("Static: " + myFirstObject.STATIC_NUMBER + ","
+                + mySecondObject.STATIC_NUMBER + ","
+                + MyObject.STATIC_NUMBER);
     }
 }
---
+++ src/com/company/MyObject.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyObject {
+
+    static int STATIC_NUMBER = 1;
+    static String STATIC_STRING = "static string";
+
+    int number;
+    String text;
+}
